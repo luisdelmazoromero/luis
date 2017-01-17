b1d9d3c62a77c255ec57af8ce28ff52a2238a55e
Alumno <Alumno@DESKTOP-EMVQV73>
2017-01-17 13:29:04 -0600

    Tarea1

---
+++ src/tareas/Tareas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareas;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Tareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
